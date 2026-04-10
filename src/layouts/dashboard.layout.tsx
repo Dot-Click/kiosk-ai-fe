d0d4f855044b48e9567a673f38d0ca4b2213dff5
muhammedlayan12 <muhammedlayan12@gmail.com>
2026-04-10 16:12:11 +0500

    working on fixes and other

--- src/layouts/dashboard.layout.tsx
+++ src/layouts/dashboard.layout.tsx
@@ -1,13 +1,11 @@
 import { HorizontalNavbar } from "@/components/horizontalnavbar/horizontalnavbar";
 import { Box } from "@/components/ui/box";
 import { Outlet } from "react-router";
-import CurrencySwitcher from "@/components/common/CurrencySwitcher";
 
 export const DashboardLayout = () => {
   return (
     <Box>
       <HorizontalNavbar />
-      <CurrencySwitcher />
       <Outlet />
     </Box>
   );
